80ab08fea811aa0de7dd47ac02fdf6cc4ade3bdb
Tomara Petty <tomararuth@gmail.com>
2021-02-07 15:33:15 -0800

    seeded database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,11 @@
 USE burgers_db;
-INSERT INTO burgers(burgers_name, devoured)
-VALUES ("hamburger",false), ("cheeseburger",false), ("veggieburger",false);
+
+INSERT INTO burgers (burgers_name) VALUES ('Peas & Thank You Burger');
+INSERT INTO burgers (burgers_name) VALUES ('Sweet Dreams Are Made of Cheese');
+INSERT INTO burgers (burgers_name) VALUES ('I’ve Created a Muenster Burger');
+INSERT INTO burgers (burgers_name) VALUES ('Blue Is The Warmest Cheese Burger');
+INSERT INTO burgers (burgers_name) VALUES ('She’s A Super Leek');
+INSERT INTO burgers (burgers_name) VALUES ('If Looks Could Kale Burger');
+INSERT INTO burgers (burgers_name) VALUES ('Sweet Home Avocado Burger');
+INSERT INTO burgers (burgers_name) VALUES ('Olive & Let Die');
+INSERT INTO burgers (burgers_name) VALUES ('Hit Me With Your Best Shallot');
