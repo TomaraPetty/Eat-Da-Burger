07ecc4b193f36c4cfcfd6e5700161b7710b9ccb8
Tomara Petty <tomararuth@gmail.com>
2021-02-07 16:46:30 -0800

    adding orm update method and export.

--- config/orm.js
+++ config/orm.js
@@ -10,7 +10,7 @@ const orm = {
             }
             cb(res);
         })
-    }
+    },
     insertOne: function(table, cols, vals, cb) {
         var dbQuery = "INSERT INTO " +
         table +
@@ -26,5 +26,24 @@ const orm = {
             }
             cb(res)
         })
-    }
+    },
+    update: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+    
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      }
 }
+
+module.exports = orm;
