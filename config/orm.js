e750de98c7e0f5e4d85e1f949f304ede730f9a92
Tomara Petty <tomararuth@gmail.com>
2021-03-22 17:36:55 -0700

    adding orm, burger js, main handlebars, and index handlebars.

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,24 @@
-const connection = require('../config/connection')
+const connection = require('../config/connection.js');
+
+function objToSql(ob) {
+    var arr = [];
+  
+    for (var key in ob) {
+      var value = ob[key];
+      
+      if (Object.hasOwnProperty.call(ob, key)) {
+        
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        
+        arr.push(key + "=" + value);
+      }
+    }
+  
+    return arr.toString();
+  }
+
 
 const orm = {
     selectAll: function(table, cb) {
@@ -9,22 +29,26 @@ const orm = {
                 throw err;
             }
             cb(res);
-        })
+        });
     },
     insertOne: function(table, cols, vals, cb) {
         var dbQuery = "INSERT INTO " +
-        table +
-        ') ' +
-        'VALUES (' +
-        createQmarks(vals.length) +
-        ') ';
+        table;
+
+        dbQuery += " (";
+        dbQuery += cols.toString();
+        dbQuery += ") ";
+        dbQuery += "VALUES (";
+        dbQuery += printQuestionMarks(vals.length);
+        dbQuery += ") ";
 
         console.log(dbQuery);
+        
         connection.query(dbQuery, vals, function(err, res) {
             if (err) {
                 throw (err);
             }
-            cb(res)
+            cb(res);
         })
     },
     update: function(table, objColVals, condition, cb) {
@@ -36,14 +60,28 @@ const orm = {
         queryString += condition;
     
         console.log(queryString);
-        connection.query(queryString, function(err, result) {
+        connection.query(queryString, function(err, res) {
           if (err) {
             throw err;
           }
     
-          cb(result);
+          cb(res);
+        });
+      },
+      delete: function(table, condition, cb) {
+        var dbQuery = "DELETE FROM " + table;
+        dbQuery += " WHERE ";
+        dbQuery += condition;
+    
+        connection.query(dbQuery, function(err, res) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(res);
         });
       }
-}
+
+};
 
 module.exports = orm;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -28,10 +28,22 @@ router.put("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  burger.updateOne({
+  burger.update({
     devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
       return res.status(404).end();
     } else {
       res.status(200).end();
